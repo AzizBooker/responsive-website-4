168472d73cf0cebf90289e1c42eb6a626e9eb50b
Aziz Booker <bookeraziz@gmail.com>
2021-01-14 11:30:14 -0500

    Finshed hamburger menu animation and overlay

---
+++ Notes/notes.md
@@ -0,0 +1,5 @@
+#Notes
+follow along tutorial from [link to tutorial]https://www.youtube.com/watch?v=XKehq1w4vfg&list=PLUWqFDiirlsuYscECzks6zIZWr_Cfcx9k&index=2
+
+##What i learned from lesson 1cd 
+1. CSS Helper classes
--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,16 @@
-console.log('hello worl!')
+const btnHamburger=document.querySelector('#btnHamburger')
+const header=document.querySelector('.header')
+const overlay=document.querySelector('.overlay')
+
+btnHamburger.addEventListener('click',function(){
+   if(header.classList.contains('open')){//Close hamburger menu
+    
+       header.classList.remove('open')
+       overlay.classList.add('fade-out')
+       overlay.classList.remove('fade-in')
+   }else{ //Open hamburger Menu
+       header.classList.add('open')
+       overlay.classList.add('fade-in')
+       overlay.classList.remove('fade-out')
+   }
+})
