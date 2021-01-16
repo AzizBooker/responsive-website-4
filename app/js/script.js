32bd6d98f1f46b77238a42e0fdb2fce2b04bf316
Aziz Booker <bookeraziz@gmail.com>
2021-01-16 12:26:52 -0500

    Finshed mobile menu dropdown

--- app/js/script.js
+++ app/js/script.js
@@ -1,16 +1,29 @@
-const btnHamburger=document.querySelector('#btnHamburger')
-const header=document.querySelector('.header')
-const overlay=document.querySelector('.overlay')
+const btnHamburger = document.querySelector('#btnHamburger')
+const header = document.querySelector('.header')
+const overlay = document.querySelector('.overlay')
+const fadeElems = document.querySelectorAll('.has-fade')
+const body=document.querySelector('body')
 
-btnHamburger.addEventListener('click',function(){
-   if(header.classList.contains('open')){//Close hamburger menu
-    
-       header.classList.remove('open')
-       overlay.classList.add('fade-out')
-       overlay.classList.remove('fade-in')
-   }else{ //Open hamburger Menu
-       header.classList.add('open')
-       overlay.classList.add('fade-in')
-       overlay.classList.remove('fade-out')
-   }
+
+btnHamburger.addEventListener('click', function () {
+    if (header.classList.contains('open')) {//Close hamburger menu
+
+        header.classList.remove('open')
+        body.classList.remove('noscroll')
+        fadeElems.forEach(function (element) {
+
+            element.classList.add('fade-out')
+            element.classList.remove('fade-in')
+        })
+
+    } else { //Open hamburger Menu
+        header.classList.add('open')
+        body.classList.add('noscroll')
+        fadeElems.forEach(function (element) {
+
+            element.classList.add('fade-in')
+            element.classList.remove('fade-out')
+        })
+    }
 })
+
